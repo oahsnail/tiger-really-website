08a94a69edb43a426363c92167593306a4824613
Lian Shao <lianshao2000@gmail.com>
2023-01-08 14:11:00 -0800

    created navbar component

---
+++ components/navbar.js
@@ -0,0 +1,22 @@
+class Navbar extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        this.innerHTML = `
+        <link rel="stylesheet" type="text/css" href="/css/navbar.css" />
+        <header>
+            <ul class="navbar">
+                <li><a href="index.html">About</a></li>
+                <li><a href="index.html">Music</a></li>
+                <li><a href="javascript:void(0)">Tour</a></li>
+                <li><a href="javascript:void(0)">Videos</a></li>
+                <li><a href="javascript:void(0)">Press Kit</a></li>
+                <li><a href="javascript:void(0)">Contact</a></li>
+            </ul>
+        </header>
+        `;
+    }
+}
+
+customElements.define('navbar-component', Navbar);
