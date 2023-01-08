6a2347ab924e397d25f434ba7867375097d3c445
Lian Shao <lianshao2000@gmail.com>
2023-01-08 14:39:42 -0800

    small refactors

--- components/navbar.js
+++ components/navbar.js
@@ -7,10 +7,9 @@ class Navbar extends HTMLElement {
         <link rel="stylesheet" type="text/css" href="/css/navbar.css" />
         <header>
             <ul class="navbar">
-                <li><a href="index.html">About</a></li>
-                <li><a href="index.html">Music</a></li>
+                <li><a href="/../index.html">About</a></li>
+                <li><a href="/pages/music.html">Music</a></li>
                 <li><a href="javascript:void(0)">Tour</a></li>
-                <li><a href="javascript:void(0)">Videos</a></li>
                 <li><a href="javascript:void(0)">Press Kit</a></li>
                 <li><a href="javascript:void(0)">Contact</a></li>
             </ul>
