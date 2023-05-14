0821cd133ee4e43878d23dd96c59430575127a5c
Lian Shao <lianshao2000@gmail.com>
2023-05-14 05:09:06 -0700

    reverting the last 'fix' that actually broke everything

--- public/components/footer.js
+++ public/components/footer.js
@@ -4,19 +4,19 @@ class Footer extends HTMLElement {
     }
     connectedCallback() {
         this.innerHTML = `
-        <link rel="stylesheet" type="text/css" href="/public/css/components/footer.css" />
+        <link rel="stylesheet" type="text/css" href="/css/components/footer.css" />
         <div class="footer">
             <a target="_blank" href="https://open.spotify.com/artist/7v1sBydqFkxYzy93nQpqJx?si=vU7pBTShRuqPI-aOanTLLA"><img
-                src="/public/icon/spotify_black_icon.png" class="icon" alt="spotify" /></a>
+                src="/icon/spotify_black_icon.png" class="icon" alt="spotify" /></a>
             <a target="_blank" href="https://www.youtube.com/channel/UCErbKaOip6mnEL0tb9PxzKQ"><img
-                src="/public/icon/youtube_black_icon.png" class="icon" alt="youtube" /></a>
-            <a target="_blank" href="http://instagram.com/tigerreally"><img src="/public/icon/instagram_icon_black.png" class="icon"
+                src="/icon/youtube_black_icon.png" class="icon" alt="youtube" /></a>
+            <a target="_blank" href="http://instagram.com/tigerreally"><img src="/icon/instagram_icon_black.png" class="icon"
                 alt="instagram" /></a>
-            <a target="_blank" href="https://www.tiktok.com/@tigerreally?"><img src="/public/icon/tiktok_black_icon.png" class="icon"
+            <a target="_blank" href="https://www.tiktok.com/@tigerreally?"><img src="/icon/tiktok_black_icon.png" class="icon"
                 alt="tiktok" /></a>
-            <a target="_blank" href="https://soundcloud.com/tigerreally"><img src="/public/icon/soundcloud_icon.png" class="icon"
+            <a target="_blank" href="https://soundcloud.com/tigerreally"><img src="/icon/soundcloud_icon.png" class="icon"
                 alt="soundcloud" /></a>
-            <a target="_blank" href="https://tigerreally.bandcamp.com/releases"><img src="/public/icon/bandcamp-logo-png-4.png"
+            <a target="_blank" href="https://tigerreally.bandcamp.com/releases"><img src="/icon/bandcamp-logo-png-4.png"
                 class="icon" alt="bandcamp" /></a>
             <br />
         </div>
--- public/components/navbar.js
+++ public/components/navbar.js
@@ -4,14 +4,14 @@ class Navbar extends HTMLElement {
     }
     connectedCallback() {
         this.innerHTML = `
-        <link rel="stylesheet" type="text/css" href="/public/css/components/navbar.css" />
+        <link rel="stylesheet" type="text/css" href="/css/components/navbar.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <div id="topnav" class="navbar">
-            <a href="/public/index.html">About</a>
-            <a href="/public/pages/music.html">Music</a>
-            <a href="/public/pages/tour.html">Tour</a>
-            <a href="/public/pages/press.html">Press Kit</a>
-            <a href="/public/pages/contact.html">Contact</a>
+            <a href="/index.html">About</a>
+            <a href="/pages/music.html">Music</a>
+            <a href="/pages/tour.html">Tour</a>
+            <a href="/pages/press.html">Press Kit</a>
+            <a href="/pages/contact.html">Contact</a>
             <a href="javascript:void(0);" class="icon" onclick="myFunction()">
                 <i id="navIcon" class="fa fa-bars"></i>
             </a>
