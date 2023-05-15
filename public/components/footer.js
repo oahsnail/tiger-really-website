cbfea6e32db8b4680416c027fa8638e2b8d0330f
Lian Shao <lianshao2000@gmail.com>
2023-05-15 04:06:56 -0700

    removed soundcloud cuz noones used that since 2016

--- public/components/footer.js
+++ public/components/footer.js
@@ -14,8 +14,6 @@ class Footer extends HTMLElement {
                 alt="instagram" /></a>
             <a target="_blank" href="https://www.tiktok.com/@tigerreally?"><img src="/icon/tiktok_black_icon.png" class="icon"
                 alt="tiktok" /></a>
-            <a target="_blank" href="https://soundcloud.com/tigerreally"><img src="/icon/soundcloud_icon.png" class="icon"
-                alt="soundcloud" /></a>
             <a target="_blank" href="https://tigerreally.bandcamp.com/releases"><img src="/icon/bandcamp-logo-png-4.png"
                 class="icon" alt="bandcamp" /></a>
             <br />
