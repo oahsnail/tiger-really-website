42f08d8c8a84a656c54a1fa7e27dc4ab1b51f359
promaglian <lian@promagenviro.ca>
2025-10-31 01:52:19 -0700

    minor changes

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="center" style="color: white; text-shadow: 1px 1px 6px rgba(0,0,0,0.5);">
 
-        <h1>Booking:</h1>
-        <h2>Canada/US: <a href="mailto:tigerreallyband@gmail.com"
+        <h2>Booking:</h2>
+        <h3>Canada/US: <a href="mailto:tigerreallyband@gmail.com"
                 style="text-decoration: underline; color: inherit;">tigerreallyband@gmail.com</a>
-        </h2>
-        <h2>Europe: <a href="mailto:connor@just-another.co"
-                style="text-decoration: underline; color: inherit;">connor@just-another.co</a></h2>
+        </h3>
+        <h3>Europe: <a href="mailto:connor@just-another.co"
+                style="text-decoration: underline; color: inherit;">connor@just-another.co</a></h3>
         <br />
-        <h1>General Inquiries:</h1>
-        <h2><a href="mailto:tigerreallyband@gmail.com"
-                style="text-decoration: underline; color: inherit;">tigerreallyband@gmail.com</a></h2>
+        <h2>General Inquiries:</h2>
+        <h3><a href="mailto:tigerreallyband@gmail.com"
+                style="text-decoration: underline; color: inherit;">tigerreallyband@gmail.com</a></h3>
 
 
     </div>
